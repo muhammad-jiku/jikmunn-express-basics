a736ed195ef3bc4294d6402c28a41dd9010b728a
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-19 12:12:16 +0600

    app.all

--- index.js
+++ index.js
@@ -3,25 +3,11 @@ const express = require('express');
 const handle = require('./handle');
 
 const app = express(); // main app
-const admin = express(); // sub app
 const PORT = process.env.PORT || 5001;
 
-admin.on('mount', (parent) => {
-  console.log('Admin Mounted');
-  console.log(parent); // refers to the parent app
-});
-
-admin.get('/dashboard', (req, res) => {
-  console.log(admin.mountpath);
-  res.send('Hello friends of admin!!');
-});
-
-app.get('/', (req, res) => {
-  console.log(app.mountpath);
+app.all('/', (req, res) => {
   res.send('Hello there! folks!!');
 });
-
-app.use('/admin', admin);
 
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
