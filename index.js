34efaecaaf2c3f5a036eca47020b0095b8f49903
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-19 12:29:04 +0600

    app.enable and app.disable

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 // dependencies
 const express = require('express');
-const handle = require('./handle');
+// const handle = require('./handle');
 
 const app = express(); // main app
 const PORT = process.env.PORT || 5001;
 
-app.all('/', (req, res) => {
+app.enable('case sensitive routing');
+app.disable('case sensitive routing');
+
+app.all('/home', (req, res) => {
   res.send('Hello there! folks!!');
 });
+
+app.use(express());
 
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
