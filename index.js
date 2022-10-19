310f9bd2c64da25ef19d053821be5a9edf114df7
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-20 00:06:52 +0600

    res.render && ejs && res.headersSent

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-  res.send('Hello there! folks!!');
+  console.log(res.headersSent);
+  res.render('pages/home', {
+    countryCode: 'BD',
+  });
+  console.log(res.headersSent);
+});
+
+app.get('/about', (req, res) => {
+  console.log(res.headersSent);
+
+  res.render('pages/about', {
+    country: 'Bangladesh',
+  });
+  console.log(res.headersSent);
 });
 
 app.listen(PORT, () => {
