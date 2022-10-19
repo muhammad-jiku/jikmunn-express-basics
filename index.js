fbfa60b5b613e8063eb7c6c030507f5d95302a5a
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-19 11:49:00 +0600

    app.locals

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 // dependencies
 const express = require('express');
+const handle = require('./handle');
+
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.get('/', (req, res) => {
-  res.send('Hello there! folks!!');
-});
+// app.locals
+app.locals.title = 'This is TITLE!!';
+
+app.get('/', handle);
 
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
