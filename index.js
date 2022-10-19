6620df4f60a3d9ba335251df33f46d9b4c7670d4
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-19 13:39:46 +0600

    app.route

--- index.js
+++ index.js
@@ -1,25 +1,22 @@
 // dependencies
+const { application } = require('express');
 const express = require('express');
 // const handle = require('./handle');
 
 const app = express(); // main app
 const PORT = process.env.PORT || 5001;
 
-app.use(express.json());
-
-app.param('id', (req, res, next, id) => {
-  const user = {
-    id,
-    name: 'Muhammad Azizul Hoque Jiku',
-  };
-  req.userDetails = user;
-  next();
-});
-
-app.get('/user/:id', (req, res) => {
-  console.log(req.userDetails);
-  res.send('Hello there! folks!!');
-});
+app
+  .route('/home')
+  .get((req, res) => {
+    res.send('Hello there! folks this is get method!!');
+  })
+  .post((req, res) => {
+    res.send('Hello there! folks this is post method!!');
+  })
+  .put((req, res) => {
+    res.send('Hello there! folks this is put method!!');
+  });
 
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
