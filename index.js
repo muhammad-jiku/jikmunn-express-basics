10dc4b7f531e6df60359fd1de6dea06cbe718fca
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-20 00:37:39 +0600

    res.cookie

--- index.js
+++ index.js
@@ -6,45 +6,13 @@ const PORT = process.env.PORT || 5001;
 app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.format({
-    'text/plain': () => {
-      res.send('Hola amigos!');
-    },
-    'text/html': () => {
-      res.render('pages/home', {
-        countryCode: 'BD',
-      });
-    },
-    'application/json': () => {
-      res.json({
-        message: 'Homepage',
-      });
-    },
-    default: () => {
-      res.status(406).send('Not acceptable!!');
-    },
-  });
+  res.cookie('countryCode', 'BD');
+  res.end();
 });
 
 app.get('/about', (req, res) => {
-  res.format({
-    'text/plain': () => {
-      res.send('Hello there!!');
-    },
-    'text/html': () => {
-      res.render('pages/about', {
-        country: 'Bangladesh',
-      });
-    },
-    'application/json': () => {
-      res.json({
-        message: 'About page',
-      });
-    },
-    default: () => {
-      res.status(406).send('Not acceptable!');
-    },
-  });
+  res.cookie('country', 'bangladesh');
+  res.end();
 });
 
 app.listen(PORT, () => {
