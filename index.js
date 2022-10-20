a88c5da72b25b00481d3c5c6b0beb84091d0281b
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-20 10:57:07 +0600

    custom middleware added

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+const myMiddleware = (req, res, next) => {
+  console.log('I am using middleware');
+  next();
+};
+
+// middleware
+app.use(myMiddleware);
+
 app.get('/', (req, res) => {
   res.send('Hello there! folks!!');
 });
