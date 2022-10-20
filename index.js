797ff655b72ffe1f0052c1931dce72bd45c62069
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-20 11:13:21 +0600

    custom login info added to the admin router as admin info

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 // dependencies
 const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 5001;
+const app = express(); // main app
+const adminRouter = express.Router(); // sub app
 
-const myMiddleware = (req, res, next) => {
-  console.log('I am using middleware');
+const PORT = process.env.PORT || 5001;
+const logger = (req, res, next) => {
+  console.log(
+    `${new Date(Date.now()).toLocaleString()} - ${req.method} - ${
+      req.originalUrl
+    } - ${req.protocol} - ${req.ip}`
+  );
   next();
 };
 
 // middleware
-app.use(myMiddleware);
+app.use('/admin', adminRouter);
+adminRouter.use(logger);
 
 app.get('/', (req, res) => {
   res.send('Hello there! folks!!');
+});
+
+adminRouter.get('/dashboard', (req, res) => {
+  res.send("Welcome to the admin's dasboard!!");
 });
 
 app.listen(PORT, () => {
