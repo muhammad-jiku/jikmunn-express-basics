ad74f173027623022506f7dc2bee0f93b411ef39
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-20 14:14:09 +0600

    complex param method is added

--- routes/publicRouter.js
+++ routes/publicRouter.js
@@ -2,17 +2,23 @@
 const express = require('express');
 const publicRouter = express.Router();
 
-publicRouter.param('user', (req, res, next, id) => {
-  req.user = id === '1' ? 'Admin' : 'Anonymous';
-  next();
+publicRouter.param((param, option) => (req, res, next, val) => {
+  //   console.log(typeof val, typeof option);
+  if (val === option) {
+    next();
+  } else {
+    res.sendStatus(403);
+  }
 });
+
+publicRouter.param('user', '164');
 
 publicRouter.get('/', (req, res) => {
   res.send('Hello there! folks!!');
 });
 
 publicRouter.get('/:user', (req, res) => {
-  res.send(`Kire ki obostha! ${req.user}`);
+  res.send(`Kire ki obostha! `);
 });
 
 module.exports = publicRouter;
