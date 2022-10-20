914c62921138729e4b75d02ca5de86e8a84ccf1a
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-20 14:21:56 +0600

    route method added

--- routes/publicRouter.js
+++ routes/publicRouter.js
@@ -2,23 +2,23 @@
 const express = require('express');
 const publicRouter = express.Router();
 
-publicRouter.param((param, option) => (req, res, next, val) => {
-  //   console.log(typeof val, typeof option);
-  if (val === option) {
+publicRouter
+  .route('/user')
+  .all((req, res, next) => {
+    console.log('I am logging into the site');
     next();
-  } else {
-    res.sendStatus(403);
-  }
-});
-
-publicRouter.param('user', '164');
-
-publicRouter.get('/', (req, res) => {
-  res.send('Hello there! folks!!');
-});
-
-publicRouter.get('/:user', (req, res) => {
-  res.send(`Kire ki obostha! `);
-});
+  })
+  .get((req, res) => {
+    res.send('GET');
+  })
+  .post((req, res) => {
+    res.send('POST');
+  })
+  .put((req, res) => {
+    res.send('PUT');
+  })
+  .delete((req, res) => {
+    res.send('DELETE');
+  });
 
 module.exports = publicRouter;
