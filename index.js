3274f10580268b42c98743fbd96ef5608eb4eb45
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-21 22:14:24 +0600

    uploading single file

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 // dependencies
 const express = require('express');
+const multer = require('multer');
+
 const app = express();
 const PORT = process.env.PORT || 5001;
 
+// file upload folders
+const UPLOADS_FOLDER = './uploads/';
+
+let upload = multer({
+  dest: UPLOADS_FOLDER,
+});
+
 app.get('/', (req, res) => {
   res.send('Hello there! folks!!');
+});
+
+app.post('/', upload.single('avatar'), (req, res) => {
+  res.send('Hola, This is US!!');
 });
 
 app.listen(PORT, () => {
