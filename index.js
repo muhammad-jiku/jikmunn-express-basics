98c080a1fbe2381c11b232ea5eb1447b8ec3869e
muhammad-jiku <muhammadjiku364@gmail.com>
2022-10-21 22:35:35 +0600

    receiving name as form data

--- index.js
+++ index.js
@@ -16,16 +16,9 @@ app.get('/', (req, res) => {
   res.send('Hello there! folks!!');
 });
 
-app.post(
-  '/',
-  upload.fields([
-    { name: 'avatar', maxCount: 01 },
-    { name: 'gallery', maxCount: 03 },
-  ]),
-  (req, res) => {
-    res.send('Hola, This is US!!');
-  }
-);
+app.post('/', upload.none(), (req, res) => {
+  res.send('Hola, This is US!!');
+});
 
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
